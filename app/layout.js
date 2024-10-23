6d7376d3456d3f5e2212ad4f131dd75abc343790
thimmaiah <thimmaiahmp72@gmail.com>
2024-10-23 20:44:10 +0530

    use of Script tag

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import { Inter } from 'next/font/google'
 import './globals.css'
 
@@ -12,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script src="https://kit.fontawesome.com/f33568eb55.js" crossOrigin="anonymous"></script>
+        <Script src="https://kit.fontawesome.com/f33568eb55.js" crossOrigin="anonymous" strategy="lazyOnload"></Script>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
